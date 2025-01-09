ff4cee38e09a0c3ed799f379b03376b8e521b705
Halidi Selemani <developer@exactmanpower.co.tz>
2025-01-09 11:18:56 +0300

    update change

--- index.js
+++ index.js
@@ -4,37 +4,40 @@ const bodyParser = require("body-parser");
 const cors = require('cors');
 const { chromium } = require('playwright');
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 app.use("/files", express.static("files"));
 app.use(bodyParser.text({ type: "/" }));
 
 app.get("/generatePdf", async (req, res) => {
     try {
-       const {uuid,template,name} = req.query;
+        const { uuid, template, name } = req.query;
+        const url = `https://cvbuilder.ekazi.co.tz/template${template}/${uuid}`;
+        console.log(`Navigating to: ${url}`);
+
         const browser = await chromium.launch();
         const page = await browser.newPage();
-        await page.goto(`https://cvbuilder.ekazi.co.tz/template${template}/${uuid}`);
-        await page.waitForSelector('#data');
-        await page.waitForTimeout(3000)
-        await page.pdf({ path: `files/${name} CV.pdf`, format: 'A4',printBackground: true,preferCSSPageSize:true,outline:true,margin:{bottom:30,top:template==1?30:0} });
+
+        await page.goto(url);
+        const isDataPresent = await page.evaluate(() => !!document.querySelector('#data'));
+        if (!isDataPresent) {
+            
+            throw new Error('The #data element is not present on the page .');
+        }
+
+        await page.waitForSelector('#data', { timeout: 60000 });
+        const filePath = `files/${name} CV.pdf`;
+        await page.pdf({ path: filePath, format: 'A4', printBackground: true });
         await browser.close();
-        res.status(200).json({
-            status: true,
-            body: {
-                link: `https://cvtemplate.ekazi.co.tz/files/${name} CV.pdf`
-            }
-        });
-        console.log('Completed')
+
+        res.status(200).json({ status: true, body: { link: `https://cvtemplate.ekazi.co.tz/${filePath}` } });
     } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            status: false,
-            error
-        });
+        console.error('Error:', error);
+        res.status(500).send({ status: false, message: 'Failed to generate PDF', error: error.message });
     }
 });
 
-app.listen(5000, () => {
-    console.log("Ekazi server started at 5000");
+
+app.listen(6000, () => {
+    console.log("Ekazi server started at 6000");
 });
