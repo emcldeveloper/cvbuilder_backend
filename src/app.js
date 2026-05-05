2b83c25ef395b4d8c46eb57a4bd3ae075c842035
Ibrahim Herith <ibrahimherith19@gmail.com>
2026-05-05 16:52:41 +0300

    Added cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import pdfRoutes from "./routes/pdf.routes.js";
 const app = express();
 
 // Middleware
-// app.use(cors({ origin: ["http://localhost:3000", "https://www.ekazi.com"] }));
+app.use(cors({ origin: ["http://localhost:3000", "https://www.ekazi.com"] }));
 app.use(express.json({ limit: "10mb" }));
 
 // Routes
